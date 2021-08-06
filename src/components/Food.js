c5795ca5487410b6db45bc8d06f7cb98601e2ca9
fespinosa <fjevictoriano@yahoo.com>
2021-08-06 19:43:08 -0400

    Fix class html attribute to jsx className.

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,21 +1,21 @@
 const Food = ({ image, description, servingSize, proteinQuantity }) => {
   return (
-    <div class="card shadow-lg compact side bg-base-100">
-      <div class="flex-row items-center space-x-4 card-body">
+    <div className="card shadow-lg compact side bg-base-100">
+      <div className="flex-row items-center space-x-4 card-body">
         <div>
-          <div class="avatar">
-            <div class="rounded-full w-14 h-14 shadow">
+          <div className="avatar">
+            <div className="rounded-full w-14 h-14 shadow">
               <img src={image} />
             </div>
           </div>
         </div>
-        <div class="flex-1">
-          <h2 class="card-title">{description}</h2>
-          <p class="text-base-content text-opacity-40">{servingSize}</p>
+        <div className="flex-1">
+          <h2 className="card-title">{description}</h2>
+          <p className="text-base-content text-opacity-40">{servingSize}</p>
         </div>
-        <div class="flex flex-col">
-          <p class="text-base-content">{proteinQuantity}</p>
-          <p class="text-base-content">Protein</p>
+        <div className="flex flex-col">
+          <p className="text-base-content">{proteinQuantity}</p>
+          <p className="text-base-content">Protein</p>
         </div>
       </div>
     </div>

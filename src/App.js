f0612b730d9dfdd55ff524496cbe1f1566547c79
fespinosa <fjevictoriano@yahoo.com>
2021-08-11 20:58:47 -0400

    Refactor Header to be a composite component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import AddFood from "./components/AddFood";
+import SearchFood from "./components/SearchFood";
+import DaySelect from "./components/DaySelect";
 
 function App() {
   return (
     <div>
       <Router>
         <Switch>
+          <Route path="/search">
+            <SearchFood />
+          </Route>
           <Route path="/addFood">
             <AddFood />
           </Route>
           <Route path="/">
-            <Header />
+            <Header>
+              <DaySelect />
+            </Header>
             <Footer />
           </Route>
         </Switch>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import DaySelect from "./DaySelect";
 
-const Header = () => {
+const Header = ({ children }) => {
   return (
     <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
       <div className="navbar-start"></div>
-      <div className="navbar-center ">
-        <DaySelect />
-      </div>
+      <div className="navbar-center ">{children}</div>
       <div className="navbar-end"></div>
     </div>
   );

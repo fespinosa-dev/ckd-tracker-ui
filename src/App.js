a6785ca6d78ce561ea55a08d65508056cb5b80d0
fespinosa <fjevictoriano@yahoo.com>
2021-08-11 20:27:36 -0400

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import logo from "./logo.svg";
-
 import "./App.css";
-import DaySelect from "./components/DaySelect";
-import Food from "./components/Food";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import AddFood from "./components/AddFood";
 
 function App() {
   return (
     <div>
-      <Header />
-
-      <Footer />
+      <Router>
+        <Switch>
+          <Route path="/addFood">
+            <AddFood />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Footer />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
